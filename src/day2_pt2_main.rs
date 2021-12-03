6bf83b413472ea61231c499a3aef5a4d382332fe
Sammons <ben@sammons.io>
2021-12-02 20:38:41 -0600

    day2

--- src/day2_pt2_main.rs
+++ src/day2_pt2_main.rs
@@ -3,6 +3,7 @@ fn run() -> Result<(), String> {
   let str = std::fs::read_to_string("./src/input.txt").map_err(|e| e.to_string())?;
   let mut x_pos = 0;
   let mut y_pos = 0;
+  let mut aim = 0;
   let _: Vec<()> = str.split("\n").flat_map(|l: &str| -> Option<()> {
     let pieces: Vec<&str> = l.trim().split_ascii_whitespace().collect();
     if pieces.len() != 2 {
@@ -11,9 +12,12 @@ fn run() -> Result<(), String> {
     let cmd = pieces[0];
     let value = pieces[1].parse::<i32>().ok()?;
     match cmd {
-      "forward" => x_pos += value,
-      "up" => y_pos -= value,
-      "down" => y_pos += value,
+      "forward" => {
+        x_pos += value;
+        y_pos += aim * value;
+      },
+      "up" => aim -= value,
+      "down" => aim += value,
       _ => ()
     };
     return None;
